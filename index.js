9804645dc884d07916dbe5eb4b01c595e3233b74
Masafumi Koba <ybiquitous@gmail.com>
2019-12-16 22:18:42 +0900

    Apply `prettier --write`

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-const core = require('@actions/core');
-const wait = require('./wait');
-
+const core = require("@actions/core");
+const wait = require("./wait");
 
 // most @actions toolkit packages have async methods
 async function run() {
-  try { 
-    const ms = core.getInput('milliseconds');
-    console.log(`Waiting ${ms} milliseconds ...`)
+  try {
+    const ms = core.getInput("milliseconds");
+    console.log(`Waiting ${ms} milliseconds ...`);
 
-    core.debug((new Date()).toTimeString())
+    core.debug(new Date().toTimeString());
     wait(parseInt(ms));
-    core.debug((new Date()).toTimeString())
+    core.debug(new Date().toTimeString());
 
-    core.setOutput('time', new Date().toTimeString());
-  } 
-  catch (error) {
+    core.setOutput("time", new Date().toTimeString());
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run()
+run();
