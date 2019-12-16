853f14ad89e3636458073e0e4454d03179d1af62
Masafumi Koba <ybiquitous@gmail.com>
2019-12-16 22:40:08 +0900

    Update ESLint and its settings

--- wait.js
+++ wait.js
@@ -1,5 +1,5 @@
 let wait = function(milliseconds) {
-  return new Promise((resolve, reject) => {
+  return new Promise((resolve) => {
     if (typeof milliseconds !== "number") {
       throw new Error("milleseconds not a number");
     }
