91548c59c657c145d41eff257450d2ee3c7c1355
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-02-28 01:16:45 +0900

    fix: error "npm update check failed"
    https://github.com/ybiquitous/npm-audit-fix-action/runs/471216132?check_suite_focus=true#step:2:27
    
    ```
    ┌──────────────────────────────────────────────────────────┐
    │                 npm update check failed                  │
    │           Try running with sudo or get access            │
    │           to the local update config store via           │
    │ sudo chown -R $USER:$(id -gn $USER) /home/runner/.config │
    └──────────────────────────────────────────────────────────┘
    ```

--- dist/index.js
+++ dist/index.js
@@ -2446,8 +2446,11 @@ const npmArgs = __webpack_require__(510);
 
 const NPM_VERSION = "6.14.1";
 
-module.exports = function updateNpm() {
-  return exec("sudo", ["npm", ...npmArgs("install", "--global", `npm@${NPM_VERSION}`)]);
+module.exports = async function updateNpm() {
+  await exec("sudo", ["npm", ...npmArgs("install", "--global", `npm@${NPM_VERSION}`)]);
+
+  // HACK: Fix the error "npm update check failed".
+  return exec("sudo", ["chown", "-R", `${process.env.USER}:`, `${process.env.HOME}/.config`]);
 };
 
 
