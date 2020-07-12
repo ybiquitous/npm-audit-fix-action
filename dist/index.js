bcb72984423b923d25e33da29caf4bedd0bd2e66
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2020-07-12 23:58:18 +0900

    fix: bump npm from 6.14.5 to 6.14.6 (#145)
    https://github.com/npm/cli/releases/tag/v6.14.6

--- dist/index.js
+++ dist/index.js
@@ -1114,7 +1114,7 @@ module.exports._enoent = enoent;
 
 module.exports.PACKAGE_NAME = "npm-audit-fix-action";
 module.exports.PACKAGE_URL = "https://github.com/ybiquitous/npm-audit-fix-action";
-module.exports.NPM_VERSION = "6.14.5";
+module.exports.NPM_VERSION = "6.14.6";
 
 
 /***/ }),
