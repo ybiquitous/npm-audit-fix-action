1469b6f6b30d58f5db50fd12bf5e6e8229cd1ac2
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-04-02 21:48:35 +0900

    chore: npm run package

--- dist/index.js
+++ dist/index.js
@@ -2006,7 +2006,8 @@ const isGitHubShorthand = (arg) => {
   const colonOnlyAfterHash = firstColon === -1 || (firstHash > -1 && firstColon > firstHash)
   const secondSlashOnlyAfterHash = secondSlash === -1 || (firstHash > -1 && secondSlash > firstHash)
   const hasSlash = firstSlash > 0
-  const doesNotEndWithSlash = !arg.endsWith('/')
+  // if a # is found, what we really want to know is that the character immediately before # is not a /
+  const doesNotEndWithSlash = firstHash > -1 ? arg[firstHash - 1] !== '/' : !arg.endsWith('/')
   const doesNotStartWithDot = !arg.startsWith('.')
 
   return spaceOnlyAfterHash && hasSlash && doesNotEndWithSlash && doesNotStartWithDot && atOnlyAfterHash && colonOnlyAfterHash && secondSlashOnlyAfterHash
@@ -10197,7 +10198,7 @@ gitHosts.gitlab = Object.assign({}, defaults, {
   tarballtemplate: ({ domain, user, project, committish }) => `https://${domain}/${user}/${project}/repository/archive.tar.gz?ref=${maybeEncode(committish) || 'master'}`,
   extract: (url) => {
     const path = url.pathname.slice(1)
-    if (path.includes('/-/')) {
+    if (path.includes('/-/') || path.includes('/archive.tar.gz')) {
       return
     }
 
