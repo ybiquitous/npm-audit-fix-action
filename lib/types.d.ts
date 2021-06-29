776fd185f3f918ecd7767b64033f5b59b29c8d27
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2021-06-29 15:47:19 +0900

    refactor: types

--- lib/types.d.ts
+++ lib/types.d.ts
@@ -29,18 +29,19 @@ type AuditFix = {
 
 type UrlInfo = { name: string; url: string; type: string };
 
+type PackageInfo = { name: string; version: string; location: string | null };
+
 type Report = {
-  added: Array<{ name: string; version: string; location: string | null }>;
-  removed: Array<{ name: string; version: string; location: string | null }>;
-  updated: Array<{
-    name: string;
-    version: string;
-    location: string | null;
-    previousVersion: string;
-    severity: string | null;
-    title: string | null;
-    url: string | null;
-  }>;
+  added: Array<PackageInfo>;
+  removed: Array<PackageInfo>;
+  updated: Array<
+    PackageInfo & {
+      previousVersion: string;
+      severity: string | null;
+      title: string | null;
+      url: string | null;
+    }
+  >;
   packageCount: number;
   packageUrls: Record<string, UrlInfo>;
 };
