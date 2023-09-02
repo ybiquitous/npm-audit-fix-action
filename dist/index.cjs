48c91c3ac0b8291beb39d0f411f2042324ba39e7
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2023-09-02 09:12:54 +0900

    chore: debug

--- dist/index.cjs
+++ dist/index.cjs
@@ -10962,7 +10962,8 @@ async function run() {
   core2.info(`Running on Node.js ${import_node_process.default.version}`);
   const npmVersion = await core2.group(`Update npm to ${NPM_VERSION}`, () => updateNpm(NPM_VERSION));
   await (0, import_exec7.exec)("npm", ["version"]);
-  await (0, import_exec7.exec)("where", ["node"]);
+  await (0, import_exec7.exec)("which", ["npm"]);
+  await (0, import_exec7.exec)("which", ["node"]);
   await core2.group("Install user packages", async () => {
     await (0, import_exec7.exec)("npm", npmArgs("ci"));
   });
