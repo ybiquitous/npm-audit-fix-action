c7869b0f346359cef70c37de5c1dae36ca647ff9
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2026-09-20 22:36:34 +0900

    fix: remove debug and enable no-shadow ESLint rule

--- dist/index.js
+++ dist/index.js
@@ -39601,14 +39601,12 @@ async function run() {
     await group(`Process directory: "${targetDir}"`, async () => {
       process.chdir(targetDir);
       const { report, files: newFiles } = await processDir();
-      info(`newFiles: ${JSON.stringify(newFiles)}`); // debug
       reports.push(report);
       files.push(...newFiles.map((file) => external_node_path_namespaceObject.posix.join(targetDir, file)));
     });
 
     process.chdir(baseDir);
   }
-  info(`files: ${JSON.stringify(files)}`); // debug
 
   const report = mergeReports(reports);
   if (report.packageCount === 0) {
